d4cfc62468523238d467a418710021fca6bfa4f6
fluke95 <tomaszewskijakub1995@gmail.com>
2020-03-08 19:23:22 +0100

    new interface

--- global.R
+++ global.R
@@ -1,26 +1,6 @@
-# libraries
-# shiny
+# Load packages
 library(shiny)
-library(shinydashboard)
-library(shinydashboardPlus)
-library(shinycustomloader)
-library(shinydashboardPlus)
-library(shinythemes)
-library(shinyjs)
 library(shinyWidgets)
-library(shinycssloaders)
-# other
-library(spotifyr)
-library(dplyr)
-library(DT)
-library(ggplot2)
+library(bs4Dash)
 library(plotly)
-
-source("functions.R")
-
-# credentials
-credentials <- read.csv("credentials.csv")
-access_token <- get_spotify_access_token(
-  client_id = as.character(credentials$SPOTIFY_CLIENT_ID[1]),
-  client_secret = as.character(credentials$SPOTIFY_CLIENT_SECRET[1])
-)
+library(echarts4r)
--- ui.R
+++ ui.R
@@ -1,118 +1,178 @@
-shinydashboardPlus::dashboardPagePlus(
-  skin = "green",
-  header = shinydashboardPlus::dashboardHeaderPlus(
-    title = "Spotify Artist Features"
+bs4DashPage(
+  sidebar_collapsed = FALSE,
+  controlbar_collapsed = FALSE,
+  enable_preloader = TRUE,
+  loading_duration = 1,
+  controlbar_overlay = FALSE,
+  loading_background = "DarkOliveGreen",
+  # ............................................................................
+  # navbar ---------------------------------------------------------------------
+  navbar = bs4DashNavbar(
+    status = "gray-light",
+    skin = "dark",
+    fixed = TRUE#,
+    # rightUi = tagList(
+    #   bs4UserMenu(
+    #     name = "Divad Nojnarg", 
+    #     status = "primary",
+    #     # src = "https://adminlte.io/themes/AdminLTE/dist/img/user2-160x160.jpg", 
+    #     src = NULL,
+    #     title = "What's new",
+    #     subtitle = "Author", 
+    #     footer = p("The footer", class = "text-center"),
+    #     "Updates"
+    #   )
+    # )
   ),
   # ............................................................................
-  # sidebar                                                                 ----
-  shinydashboard::dashboardSidebar( 
-    shinydashboard::sidebarMenu(
-      id = "sidebar_menu",
-      # help
-      actionLink("help_button", "Help", icon = icon("asterisk")),
-      # type artist name
-      textInput(
-        inputId = "searchArtistInput",
-        label = "Search for artists",
-        placeholder = "for example: Nickelback"
+  # sidebar --------------------------------------------------------------------
+  sidebar = bs4DashSidebar(
+    expand_on_hover = TRUE,
+    skin = "dark",
+    status = "olive",
+    title = h4(id="h4-main-title", "Spotify Artist Features"),
+    tags$style(HTML("#h4-main-title{color: mediumseagreen;}")),
+    brandColor = NULL,
+    # src = "https://adminlte.io/themes/AdminLTE/dist/img/user2-160x160.jpg",
+    src = NULL,
+    elevation = 3,
+    opacity = 0.8,
+    bs4SidebarMenu(
+      id = "current_tab",
+      flat = FALSE,
+      compact = TRUE,
+      child_indent = FALSE,
+      # bs4SidebarHeader("Cards"),
+      bs4SidebarMenuItem(
+        selectizeInput(
+          inputId = "artistsInput",
+          label = "Select artists",
+          choices = " ",
+          multiple = TRUE,
+        ),
+        text = "Galleries",
+        icon = "cubes",
+        startExpanded = FALSE
       ),
-      # pick an artist
-      selectizeInput(
-        inputId = "artistsInput",
-        label = "Select artists",
-        choices = " ",
-        multiple = TRUE,
-      ),
-      # add selected artists for analysis
-      actionButton(inputId = "AddArtist_button", 
-                   label = "Add Artist(s)"),
-      selectizeInput(
-        inputId = "selectedArtistsInput",
-        label = "Selected artists",
-        choices = " ",
-        multiple = TRUE,
-        options = list(
-          'plugins' = list('remove_button'),
-          'create' = TRUE,
-          'persist' = FALSE)
-      ),
-      
-      actionButton(inputId = "go_button", "Go!"),
-      # ........................................................................
-      # plots panel                                                         ----
-      shinydashboard::menuItem(
+      bs4SidebarMenuItem(
         "Visualization",
         tabName = "PLOT",
-        icon = shiny::icon("bar-chart")
+        icon = "sliders"
       ),
-      hidden(
-        div(
-          id = "plot_customization"#,
-          # shinyWidgets::pickerInput(
-          #   inputId = "",
-          #   label = "",
-          #   choices = "",
-          #   multiple = TRUE,
-          #   options = pickerOptions(
-          #     `actionsBox` = TRUE,
-          #     `liveSearch`  = TRUE,
-          #     liveSearchPlaceholder = "")
-          # ),
-          # actionButton(inputId = "showPlots_button", "Show Plots")
-        )
-      ),
-      # ........................................................................
-      # table panel                                                         ----
-      shinydashboard::menuItem(
+      bs4SidebarMenuItem(
         "Table",
         tabName = "TABLE",
-        icon = shiny::icon("dashboard")
-      ),
-      hidden(
-        div(
-          id = "table_customization"#,
-          # actionButton(inputId = "showTable_button", "Show Table")
-        )
+        icon = "suitcase"
       )
     )
   ),
   # ............................................................................
-  # body                                                                    ----
-  shinydashboard::dashboardBody(
-    shinyjs::useShinyjs(),
-    tags$head(includeHTML(("google-analytics.html"))),
-    shinydashboard::tabItems(
-      shinydashboard::tabItem(
+  # controlbar -----------------------------------------------------------------
+  controlbar = bs4DashControlbar(
+    inputId = "controlbar",
+    expand_on_hover = TRUE,
+    skin = "dark",
+    title = "My right sidebar",
+    width = 300,
+    setSliderColor(sliderId = 1, "black"),
+    sliderInput(
+      inputId = "obs", 
+      label = "Number of observations:",
+      min = 0, 
+      max = 1000, 
+      value = 500
+    ),
+    column(
+      width = 12,
+      align = "center",
+      radioButtons(
+        inputId = "dist", 
+        label = "Distribution type:",
+        c("Normal" = "norm",
+          "Uniform" = "unif",
+          "Log-normal" = "lnorm",
+          "Exponential" = "exp")
+      )
+    )
+  ),
+  # ............................................................................
+  # body -----------------------------------------------------------------------
+  body = bs4DashBody(
+    bs4TabItems(
+      #  . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+      # visualization ----------------------------------------------------------
+      bs4TabItem(
         tabName = "PLOT",
         fluidRow(
-          shinydashboardPlus::gradientBox(
-            plotlyOutput("scatter_plot"),
-            title = "Scatter",
-            width = 12,
-            icon = "fa fa-line-chart",
-            boxToolSize = "md"
+          bs4Card(
+            title = "Closable card with dropdown", 
+            closable = FALSE, 
+            elevation = 4,
+            width = 6,
+            status = "warning", 
+            solidHeader = FALSE, 
+            maximizable = TRUE, 
+            collapsible = TRUE,
+            dropdownMenu = dropdownItemList(
+              dropdownItem(url = "https://www.google.com", name = "Link to google"),
+              dropdownItem(url = "#", name = "item 2"),
+              dropdownDivider(),
+              dropdownItem(url = "#", name = "item 3")
+            ),
+            plotOutput("plot")
           ),
-          shinydashboardPlus::gradientBox(
-            plotlyOutput("density_plot"),
-            title = "Density",
-            width = 12,
-            icon = "fa fa-line-chart",
-            boxToolSize = "md"
+          bs4Card(
+            title = "Closable card with dropdown", 
+            closable = FALSE, 
+            elevation = 4,
+            width = 6,
+            status = "warning", 
+            solidHeader = FALSE, 
+            maximizable = TRUE, 
+            collapsible = TRUE,
+            # labelStatus = NULL,
+            # labelText = NULL,
+            # labelTooltip = NULL,
+            bs4Ribbon(
+              text = "Plot 2",
+              status = "danger",
+              size = "xl"
+            ),
+            dropdownMenu = dropdownItemList(
+              dropdownItem(url = "https://www.google.com", name = "Link to google"),
+              dropdownItem(url = "#", name = "item 2"),
+              dropdownDivider(),
+              dropdownItem(url = "#", name = "item 3")
+            ),
+            plotOutput("plot")
           )
         )
       ),
-      shinydashboard::tabItem(
+      #  . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+      # table ------------------------------------------------------------------
+      bs4TabItem(
         tabName = "TABLE",
+        h4("Value Boxes"),
         fluidRow(
-          shinydashboardPlus::gradientBox(
-            DT::dataTableOutput("rawStatsTable"),
-            title = "Table",
-            width = 12,
-            icon = "fa fa-line-chart",
-            boxToolSize = "md"
+          bs4ValueBox(
+            value = 150,
+            subtitle = "New orders",
+            status = "primary",
+            icon = "shopping-cart",
+            href = "#"
+          )
+        ),
+        h4("Info Boxes"),
+        fluidRow(
+          bs4InfoBox(
+            tabName = "cardsAPI",
+            title = "Navigate to Cards API section",
+            value = 1410,
+            icon = "laptop-code"
           )
         )
       )
+      ##
     )
   )
 )
